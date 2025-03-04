d9fc3e3385f94f794449759b178ccec8e61afe3b
NaMinhyeok <nmh9097@gmail.com>
2025-03-04 17:12:03 +0900

    서블릿 컨테이너 띄우기, 등록, 요청 처리

--- src/main/java/org/nmh/tobyspringboot/TobyspringbootApplication.java
+++ src/main/java/org/nmh/tobyspringboot/TobyspringbootApplication.java
@@ -1,13 +1,36 @@
 package org.nmh.tobyspringboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
-@SpringBootApplication
+import java.io.IOException;
+
 public class TobyspringbootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TobyspringbootApplication.class, args);
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    String name = req.getParameter("name");
+
+                    resp.setStatus(HttpStatus.OK.value());
+                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                    resp.getWriter().println("Hello " + name);
+                }
+            }).addMapping("/hello");
+
+        });
+        webServer.start();
     }
 
 }
