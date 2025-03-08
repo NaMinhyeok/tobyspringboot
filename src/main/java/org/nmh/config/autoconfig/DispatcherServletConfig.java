b896a16bc516398a7a0f44263e2e040ca00b0d71
NaMinhyeok <nmh9097@gmail.com>
2025-03-08 20:49:59 +0900

    Configuration과 prodxyBeanMethod

--- src/main/java/org/nmh/config/autoconfig/DispatcherServletConfig.java
+++ src/main/java/org/nmh/config/autoconfig/DispatcherServletConfig.java
@@ -1,11 +1,12 @@
 package org.nmh.config.autoconfig;
 
+import org.nmh.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@MyAutoConfiguration
 public class DispatcherServletConfig {
     @Bean
     public ServletWebServerFactory servletWebServerFactory() {
