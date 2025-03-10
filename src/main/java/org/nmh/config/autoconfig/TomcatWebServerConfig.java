c6aa9fc9dda753658eaf94319dae151da2e14da1
NaMinhyeok <nmh9097@gmail.com>
2025-03-10 16:24:55 +0900

    자동 구성 정보 대체

--- src/main/java/org/nmh/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/org/nmh/config/autoconfig/TomcatWebServerConfig.java
@@ -2,6 +2,7 @@ package org.nmh.config.autoconfig;
 
 import org.nmh.config.ConditionalMyOnClass;
 import org.nmh.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
     @Bean("tomcatWebServerFactory")
+    @ConditionalOnMissingBean
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
     }
