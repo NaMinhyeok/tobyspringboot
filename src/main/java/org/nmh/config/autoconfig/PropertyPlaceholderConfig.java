8351854084f10e2d19ba05f85d6541b9b9de2be8
NaMinhyeok <nmh9097@gmail.com>
2025-03-11 19:20:32 +0900

    @Value와 PropertySourcePlaceholderConfigurer

---
+++ src/main/java/org/nmh/config/autoconfig/PropertyPlaceholderConfig.java
@@ -0,0 +1,13 @@
+package org.nmh.config.autoconfig;
+
+import org.nmh.config.MyAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@MyAutoConfiguration
+public class PropertyPlaceholderConfig {
+    @Bean
+    PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
