661357bb034803986f4fe303985192e2a60f6173
NaMinhyeok <nmh9097@gmail.com>
2025-03-14 16:03:48 +0900

    jdbctemplate와 트랜잭션 매니저 구성

--- src/test/java/org/nmh/tobyspringboot/DataSourceTest.java
+++ src/test/java/org/nmh/tobyspringboot/DataSourceTest.java
@@ -1,20 +1,13 @@
 package org.nmh.tobyspringboot;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.nmh.config.autoconfig.DataSourceConfig;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = TobyspringbootApplication.class)
-@TestPropertySource("classpath:/application.properties")
+@HelloBootTest
 public class DataSourceTest {
     @Autowired
     DataSource datasource;
---
+++ src/test/java/org/nmh/tobyspringboot/JdbcTemplateTest.java
@@ -0,0 +1,27 @@
+package org.nmh.tobyspringboot;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@HelloBootTest
+public class JdbcTemplateTest {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void setUp() {
+        jdbcTemplate.execute("create table if not exists hello(name varchar(50) primary key, count int)");
+    }
+
+    @Test
+    void insertAndQuery() {
+        jdbcTemplate.update("insert into hello values(?, ?)", "Minhyeok", 3);
+        jdbcTemplate.update("insert into hello values(?, ?)", "Spirng", 1);
+
+        Long count = jdbcTemplate.queryForObject("select count(*) from hello", Long.class);
+        Assertions.assertThat(count).isEqualTo(2);
+    }
+}
